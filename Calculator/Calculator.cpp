34e3a48216195b3ebb748bbbd906027615fffdc7
mimif10 <94880765+mimif10@users.noreply.github.com>
2022-06-13 02:31:13 -0400

    Added button0 to OnButtonClicked event and updated calculator

--- Calculator/Calculator.cpp
+++ Calculator/Calculator.cpp
@@ -15,9 +15,10 @@ wxBEGIN_EVENT_TABLE(Calculator, wxFrame) // takes name of the class for the even
 	EVT_BUTTON(1000, OnButtonClicked)
 	EVT_BUTTON(1100, OnButtonClicked)
 	EVT_BUTTON(1200, OnButtonClicked)
+	EVT_BUTTON(0, OnButtonClicked)
 	EVT_BUTTON(1, OnButtonClicked)
 	EVT_BUTTON(2, OnButtonClicked)
-	EVT_BUTTON(2, OnButtonClicked)
+	EVT_BUTTON(3, OnButtonClicked)
 	EVT_BUTTON(4, OnButtonClicked)
 	EVT_BUTTON(5, OnButtonClicked)
 	EVT_BUTTON(6, OnButtonClicked)
@@ -27,40 +28,43 @@ wxBEGIN_EVENT_TABLE(Calculator, wxFrame) // takes name of the class for the even
 	wxEND_EVENT_TABLE()
 
 	// Window Frame requires some information in order to initialize it (parameters: parent, ID, name, location point, size)
-	Calculator::Calculator() : wxFrame(nullptr, wxID_ANY, "Calculator", wxPoint(720, 250), wxSize(320, 315))
+	Calculator::Calculator() : wxFrame(nullptr, wxID_ANY, "Calculator", wxPoint(720, 250), wxSize(320, 340))
 {
 	// create a textbox at the top of the window (parameters: parent (wxFrame), ID, title, starting point, starting size
-	textBox = new wxTextCtrl(this, 100, "", wxPoint(30, 26), wxSize(250, 24));
+	textBox = new wxTextCtrl(this, 100, "", wxPoint(30, 26), wxSize(250, 35));
 
 	// create a button from Calculator.h (parameters: parent (wxFrame), ID, title, starting point, starting size
 	// 1st row buttons
-	clearBtn = new wxButton(this, 200, "clear", wxPoint(30, 50), wxSize(50, 50));
-	negBtn = new wxButton(this, 1200, "(-)", wxPoint(80, 50), wxSize(50, 50));
-	modBtn = new wxButton(this, 1100, "mod", wxPoint(130, 50), wxSize(50, 50));
-	equalBtn = new wxButton(this, 1000, "=", wxPoint(180, 50), wxSize(50, 50));
+	binBtn = new wxButton(this, 500, "bin", wxPoint(30, 50), wxSize(50, 50));
+	negBtn = new wxButton(this, 600, "(-)", wxPoint(80, 50), wxSize(50, 50));
+	modBtn = new wxButton(this, 700, "mod", wxPoint(130, 50), wxSize(50, 50));
+	equalBtn = new wxButton(this, 800, "=", wxPoint(180, 50), wxSize(50, 50));
 	divideBtn = new wxButton(this, 900, "/", wxPoint(230, 50), wxSize(50, 50));
 
 	// 2nd row buttons
-	binBtn = new wxButton(this, 300, "bin", wxPoint(30, 100), wxSize(50, 50));
+	hexBtn = new wxButton(this, 400, "hex", wxPoint(30, 100), wxSize(50, 50));
 	button7 = new wxButton(this, 7, "7", wxPoint(80, 100), wxSize(50, 50));
 	button8 = new wxButton(this, 8, "8", wxPoint(130, 100), wxSize(50, 50));
 	button9 = new wxButton(this, 9, "9", wxPoint(180, 100), wxSize(50, 50));
-	mutliplyBtn = new wxButton(this, 400, "*", wxPoint(230, 100), wxSize(50, 50));
+	mutliplyBtn = new wxButton(this, 1000, "*", wxPoint(230, 100), wxSize(50, 50));
 
 	// 3rd row buttons
-	hexBtn = new wxButton(this, 400, "hex", wxPoint(30, 150), wxSize(50, 50));
+	decBtn = new wxButton(this, 300, "dec", wxPoint(30, 150), wxSize(50, 50));
 	button4 = new wxButton(this, 4, "4", wxPoint(80, 150), wxSize(50, 50));
 	button5 = new wxButton(this, 5, "5", wxPoint(130, 150), wxSize(50, 50));
 	button6 = new wxButton(this, 6, "6", wxPoint(180, 150), wxSize(50, 50));
-	substractBtn = new wxButton(this, 700, "-", wxPoint(230, 150), wxSize(50, 50));
+	substractBtn = new wxButton(this, 1100, "-", wxPoint(230, 150), wxSize(50, 50));
 
 	// 4th row buttons
-	decBtn = new wxButton(this, 500, "dec", wxPoint(30, 200), wxSize(50, 50));
 	button0 = new wxButton(this, 0, "0", wxPoint(30, 200), wxSize(50, 50));
 	button1 = new wxButton(this, 1, "1", wxPoint(80, 200), wxSize(50, 50));
 	button2 = new wxButton(this, 2, "2", wxPoint(130, 200), wxSize(50, 50));
 	button3 = new wxButton(this, 3, "3", wxPoint(180, 200), wxSize(50, 50));
-	addBtn = new wxButton(this, 600, "+", wxPoint(230, 200), wxSize(50, 50));
+	addBtn = new wxButton(this, 1200, "+", wxPoint(230, 200), wxSize(50, 50));
+
+	// 5th row buttons
+	clearBtn = new wxButton(this, 200, "clear", wxPoint(30, 250), wxSize(250, 30));
+
 }
 
 Calculator::~Calculator()
@@ -75,6 +79,18 @@ void Calculator::OnButtonClicked(wxCommandEvent& event)
 	// Use a Switch Statement for each button
 	switch (id)
 	{
+	case 0:
+	{
+		/*if (textBox->GetValue() == "0")
+		{*/
+			textBox->SetValue(button0->GetLabel());
+		/*}
+		else
+		{
+			textBox->AppendText("0");
+		}*/
+		break;
+	}
 	case 1:
 	{
 		if (textBox->GetValue() == "0")
@@ -188,15 +204,15 @@ void Calculator::OnButtonClicked(wxCommandEvent& event)
 		textBox->SetValue("0");
 		break;
 	}
-	case 300: // bin butto
+	case 300: // dec button
 	{
 		if (textBox->GetValue() == "0")
 		{
-			textBox->SetValue(binBtn->GetLabel());
+			textBox->SetValue(decBtn->GetLabel());
 		}
 		else
 		{
-			textBox->AppendText("bin ");
+			textBox->AppendText("dec ");
 		}
 		break;
 	}
@@ -212,58 +228,57 @@ void Calculator::OnButtonClicked(wxCommandEvent& event)
 		}
 		break;
 	}
-	case 500: // dec button
+	case 500: // bin button
 	{
 		if (textBox->GetValue() == "0")
 		{
-			textBox->SetValue(decBtn->GetLabel());
+			textBox->SetValue(binBtn->GetLabel());
 		}
 		else
 		{
-			textBox->AppendText("dec ");
+			textBox->AppendText("bin ");
 		}
 		break;
 	}
-	case 600:
+	case 600: // neg button
 	{
-		textBox->AppendText(" + ");
+		textBox->AppendText("-");
 		break;
 	}
-	case 700:
-	{
-		textBox->AppendText(" - ");
-		break;
-	}
-	case 800:
-	{
-		textBox->AppendText(" * ");
-		break;
-	}
-	case 900:
-	{
-		textBox->AppendText(" / ");
-		break;
-	}
-	case 1000: // equal button
-	{
-		textBox->AppendText(" = ");
-		break;
-	}
-	case 1100: // mod button
+	case 700: // mod button
 	{
 		textBox->AppendText(" mod ");
 		break;
 	}
-	case 1200: // make negative button
+	case 800: // equal button
 	{
-		if (textBox->GetValue() == "0")
+		textBox->AppendText(" = ");
+		break;
+	}
+	case 900: // divide button
+	{
+		textBox->AppendText(" / ");
+		break;
+	}
+	case 1000: // multiply button
+	{
+		textBox->AppendText(" * ");
+		break;
+	}
+	case 1100: // minus button
+	{
+		/*if (textBox->GetValue() == "0")
 		{
 			textBox->SetValue(negBtn->GetLabel());
 		}
 		else
-		{
-			textBox->AppendText("-");
-		}
+		{*/
+			textBox->AppendText(" - ");
+		break;
+	}
+	case 1200: // plus button
+	{
+		textBox->AppendText(" + ");
 		break;
 	}
 	}
