feea36d3173c4aeafc3c792cc2e5d4e839911455
mimif10 <94880765+mimif10@users.noreply.github.com>
2022-06-12 20:57:47 -0400

    Add "bin"  Binary Button to the Window Frame

--- Calculator/Calculator.cpp
+++ Calculator/Calculator.cpp
@@ -7,15 +7,19 @@ Calculator::Calculator() : wxFrame(nullptr, wxID_ANY, "Calculator", wxPoint(200,
 	text1 = new wxTextCtrl(this, 100, "", wxPoint(10, 10), wxSize(300, 40));
 
 	// create a button from Calculator.h (parameters: parent (wxFrame), ID, title, starting point, starting size
-	// 3rd row
-	hexbtn = new wxButton(this, 400, "hex", wxPoint(30, 200), wxSize(50, 50));
+	
+	// 2nd row buttons
+	binBtn = new wxButton(this, 400, "bin", wxPoint(30, 150), wxSize(50, 50));
+
+	// 3rd row buttons
+	hexBtn = new wxButton(this, 400, "hex", wxPoint(30, 200), wxSize(50, 50));
 	button4 = new wxButton(this, 4, "4", wxPoint(80, 200), wxSize(50, 50));
 	button5 = new wxButton(this, 5, "5", wxPoint(130, 200), wxSize(50, 50));
 	button6 = new wxButton(this, 6, "6", wxPoint(180, 200), wxSize(50, 50));
 	SubstractBtn = new wxButton(this, 700, "-", wxPoint(230, 200), wxSize(50, 50));
 
-	// 4th row
-	decbtn = new wxButton(this, 500, "dec", wxPoint(30, 250), wxSize(50, 50));
+	// 4th row buttons
+	decBtn = new wxButton(this, 500, "dec", wxPoint(30, 250), wxSize(50, 50));
 	button1 = new wxButton(this, 1, "1", wxPoint(80, 250), wxSize(50, 50));
 	button2 = new wxButton(this, 2, "2", wxPoint(130, 250), wxSize(50, 50));
 	button3 = new wxButton(this, 3, "3", wxPoint(180, 250), wxSize(50, 50));
--- Calculator/Calculator.h
+++ Calculator/Calculator.h
@@ -15,15 +15,22 @@ private:
 	wxTextCtrl* text1 = nullptr;
 
 	// Add buttons to the window 
+	// 2nd row buttons
+	wxButton* binBtn = nullptr;
+	wxButton* button7 = nullptr;
+	wxButton* button8 = nullptr;
+	wxButton* button9 = nullptr;
+	wxButton* SubstractBtn = nullptr;
+
 	// 3rd row buttons
-	wxButton* hexbtn = nullptr;
+	wxButton* hexBtn = nullptr;
 	wxButton* button4 = nullptr;
 	wxButton* button5 = nullptr;
 	wxButton* button6 = nullptr;
 	wxButton* SubstractBtn = nullptr;
 
 	// 4th row buttons
-	wxButton* decbtn = nullptr;
+	wxButton* decBtn = nullptr;
 	wxButton* button1 = nullptr;
 	wxButton* button2 = nullptr;
 	wxButton* button3 = nullptr;
